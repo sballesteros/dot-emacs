41670578f832a58e8f78671a28090e0737f83fde
sebastien ballesteros <sebastien.ballesteros@gmail.com>
2014-11-26 11:08:17 -0500

    tweak JS config

--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -8,16 +8,17 @@
             (require 'js2-mode)
             (setq indent-line-function 'js2-indent-line)))
 
-;;too slow
+;;I find js2-mode too slow to be turned on by default
 ;;(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 ;;(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
-;;(setq js2-highlight-level 3)
-;;(setq js2-pretty-multiline-declarations t)
-;;(setq js2-mode-show-parse-errors nil)
-;;(setq js2-mode-show-strict-warnings nil)
-;;(setq js2-strict-inconsistent-return-warning nil)
-;;(setq js2-include-node-externs t)
-;;;;couchDB
-;;(setq js2-additional-externs '("require" "respond" "getRow" "emit" "send" "start" "sum" "log" "provides"))
+
+(setq js2-highlight-level 3)
+(setq js2-pretty-multiline-declarations t)
+(setq js2-mode-show-parse-errors nil)
+(setq js2-mode-show-strict-warnings nil)
+(setq js2-strict-inconsistent-return-warning nil)
+(setq js2-include-node-externs t)
+;;couchDB
+(setq js2-additional-externs '("require" "respond" "getRow" "emit" "send" "start" "sum" "log" "provides"))
 
 (provide 'init-javascript)
